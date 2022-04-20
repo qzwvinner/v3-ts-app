09112ec5b4dcbdf401da6dffbfe1fda9bed37e60
qiuzhiwen <qzwvinner@163.com>
2022-04-20 23:06:20 +0800

    login

---
+++ src/views/LoginView.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <el-form ref="ruleFormRef" :model="ruleForm" status-icon :rules="rules" label-width="120px" class="demo-ruleForm">
+      <el-form-item label="账号" prop="pass">
+        <el-input v-model="ruleForm.pass" type="password" autocomplete="off" />
+      </el-form-item>
+      <el-form-item label="Confirm" prop="checkPass">
+        <el-input v-model="ruleForm.checkPass" type="password" autocomplete="off" />
+      </el-form-item>
+      <el-form-item label="Age" prop="age">
+        <el-input v-model.number="ruleForm.age" />
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="submitForm(ruleFormRef)">Login</el-button>
+      </el-form-item>
+    </el-form>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  setup() {
+    return;
+  },
+});
+</script>
